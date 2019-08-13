75276a7f36a3a297bc7b50f8c08b4ca591e86194
Ernest MARIN <emarin@e1r7p8.42.fr>
2019-08-13 13:07:52 +0200

    matrix translate function

--- src/matrix_op.c
+++ src/matrix_op.c
@@ -6,7 +6,7 @@
 /*   By: emarin <emarin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 16:52:59 by emarin            #+#    #+#             */
-/*   Updated: 2019/08/13 12:22:06 by emarin           ###   ########.fr       */
+/*   Updated: 2019/08/13 13:05:32 by emarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,53 @@ t_matrix	*mt_mul(t_matrix *lhs, t_matrix *rhs)
 	return (res);
 }
 
-t_matrix	*mt_scale(t_matrix *mt, t_matrix *scale_vector)
+t_matrix	*mt_scale(t_matrix *mt, t_matrix *scale_vect)
 {
 	t_matrix	*scale_mt;
 	t_matrix	*res;
 
-	if (!(scale_vector->h == 3 && scale_vector->w == 1
+	if (!(scale_vect->h == 3 && scale_vect->w == 1
 	&& mt->w == 4 && mt->h == 4))
 	{
 		if (mt->w != 4 || mt->h != 4)
 			fprintf(stderr, "mt need to be a 4x4 matrix");
 		else
-			fprintf(stderr, "scale_vector need to be a 3x1 matrix");
+			fprintf(stderr, "scale_vect need to be a 3x1 matrix");
 		return (NULL);
 	}
 	if (!(scale_mt = mt_new(mt->h, mt->w, TRUE)))
 		return (NULL);
-	scale_mt->cont[0][0] = scale_vector->cont[0][0];
-	scale_mt->cont[1][1] = scale_vector->cont[1][0];
-	scale_mt->cont[2][2] = scale_vector->cont[2][0];
+	scale_mt->cont[0][0] = scale_vect->cont[0][0];
+	scale_mt->cont[1][1] = scale_vect->cont[1][0];
+	scale_mt->cont[2][2] = scale_vect->cont[2][0];
 	if (!(res = mt_mul(mt, scale_mt)))
 		return (NULL);
 	mt_free(&scale_mt);
+	return (res);
+}
+
+t_matrix	*mt_translate(t_matrix *mt, t_matrix *trans_vect)
+{
+	t_matrix	*trans_mt;
+	t_matrix	*res;
+
+	if (!(trans_vect->h == 3 && trans_vect->w == 1
+	&& mt->w == 4 && mt->h == 4))
+	{
+		if (mt->w != 4 || mt->h != 4)
+			fprintf(stderr, "mt need to be a 4x4 matrix");
+		else
+			fprintf(stderr, "trans_vect need to be a 3x1 matrix");
+		return (NULL);
+	}
+	if (!(trans_mt = mt_new(mt->h, mt->w, TRUE)))
+		return (NULL);
+	trans_mt->cont[0][3] = trans_vect->cont[0][0];
+	trans_mt->cont[1][3] = trans_vect->cont[1][0];
+	trans_mt->cont[2][3] = trans_vect->cont[2][0];
+	if (!(res = mt_mul(mt, trans_mt)))
+		return (NULL);
+	mt_free(&trans_mt);
 	return (res);
 }
 
