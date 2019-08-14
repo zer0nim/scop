18ae8f0b1477673b306e72f7e20566175897ee43
Ernest MARIN <emarin@e1r12p9.42.fr>
2019-08-14 15:20:11 +0200

    correct norm errors

--- include/matrix.h
+++ include/matrix.h
@@ -6,7 +6,7 @@
 /*   By: emarin <emarin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 15:09:07 by emarin            #+#    #+#             */
-/*   Updated: 2019/08/13 14:04:12 by emarin           ###   ########.fr       */
+/*   Updated: 2019/08/14 15:18:25 by emarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 
 # include "scop.h"
 
-typedef struct		s_matrix
+typedef struct	s_matrix
 {
 	int		w;
 	int		h;
 	float	**cont;
-}					t_matrix;
+}				t_matrix;
 
-t_matrix	*mt_new(int h, int w, int8_t is_identity);
-void		mt_print(t_matrix *mt);
-void		mt_free(t_matrix **m);
+t_matrix		*mt_new(int h, int w, int8_t is_identity);
+void			mt_print(t_matrix *mt);
+void			mt_free(t_matrix **m);
 
-t_matrix	*mt_mul(t_matrix *lhs, t_matrix *rhs);
-t_matrix	*mt_scale(t_matrix *mt, t_matrix *scale_vect);
-t_matrix	*mt_translate(t_matrix *mt, t_matrix *trans_vect);
-t_matrix	*mt_rotate(t_matrix *mt, float angle, t_matrix *axis_vect);
-void		mt_add(t_matrix *mt, float add_v);
-void		mt_scalar_prod(t_matrix *mt, float scalar_v);
+t_matrix		*mt_mul(t_matrix *lhs, t_matrix *rhs);
+t_matrix		*mt_scale(t_matrix *mt, t_matrix *scale_vect);
+t_matrix		*mt_translate(t_matrix *mt, t_matrix *trans_vect);
+t_matrix		*mt_rotate(t_matrix *mt, float angle, t_matrix *axis_vect);
+void			mt_add(t_matrix *mt, float add_v);
+void			mt_scalar_prod(t_matrix *mt, float scalar_v);
 
-t_matrix	*vect2(float x, float y);
-t_matrix	*vect3(float x, float y, float z);
-t_matrix	*vect4(float x, float y, float z, float w);
+t_matrix		*vect2(float x, float y);
+t_matrix		*vect3(float x, float y, float z);
+t_matrix		*vect4(float x, float y, float z, float w);
 
 #endif
