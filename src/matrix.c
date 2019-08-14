8f5a33c991807ad5640095f5025f92591a8c0aa6
Ernest MARIN <emarin@e1r12p9.42.fr>
2019-08-14 17:57:12 +0200

    use float * instead of float ** to store matrix content

--- src/matrix.c
+++ src/matrix.c
@@ -6,34 +6,20 @@
 /*   By: emarin <emarin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 14:10:50 by emarin            #+#    #+#             */
-/*   Updated: 2019/08/13 14:06:40 by emarin           ###   ########.fr       */
+/*   Updated: 2019/08/14 16:41:11 by emarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.h"
 #include "matrix.h"
 
-void		mt_free_cont(float ***cont, int h)
-{
-	int i;
-
-	i = -1;
-	while (++i < h)
-	{
-		if ((*cont)[i])
-		{
-			free((*cont)[i]);
-			(*cont)[i] = NULL;
-		}
-	}
-	free((*cont));
-	(*cont) = NULL;
-}
-
 void		mt_free(t_matrix **m)
 {
 	if ((*m)->cont)
-		mt_free_cont(&((*m)->cont), (*m)->h);
+	{
+		free((*m)->cont);
+		(*m)->cont = NULL;
+	}
 	if ((*m))
 	{
 		free((*m));
@@ -41,25 +27,20 @@ void		mt_free(t_matrix **m)
 	}
 }
 
-float		**mt_new_cont(int h, int w, int8_t is_identity)
+float		*mt_new_cont(int h, int w, int8_t is_identity)
 {
-	float	**cont;
+	float	*cont;
 	int		i;
 	int		j;
 
-	if (!(cont = (float **)malloc(sizeof(float *) * h)))
+	if (!(cont = (float *)malloc(sizeof(float) * w * h)))
 		return (NULL);
 	i = -1;
 	while (++i < h)
 	{
-		if (!(cont[i] = (float *)malloc(sizeof(float) * w)))
-		{
-			mt_free_cont(&cont, i);
-			return (NULL);
-		}
 		j = -1;
 		while (++j < w)
-			cont[i][j] = (is_identity && j == i) ? 1.0 : 0.0;
+			cont[i * w + j] = (is_identity && j == i) ? 1.0 : 0.0;
 	}
 	return (cont);
 }
@@ -91,7 +72,7 @@ void		mt_print(t_matrix *mt)
 	{
 		j = -1;
 		while (++j < mt->w)
-			printf("\t%.2f", mt->cont[i][j]);
+			printf("\t%.2f", mt->cont[i * mt->w + j]);
 		printf("\n");
 	}
 	printf(")\n");
