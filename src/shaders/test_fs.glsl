4532a7798cb0755a118b3e89e81bfef0e6ce1ad6
Ernest MARIN <emarin@e1r7p8.42.fr>
2019-08-16 14:30:23 +0200

    remove unused color datas

--- src/shaders/test_fs.glsl
+++ src/shaders/test_fs.glsl
@@ -1,5 +1,4 @@
 #version 410 core
-in vec3 color;
 in vec2 texCoord;
 
 out vec4 fragColor;
--- src/shaders/test_vs.glsl
+++ src/shaders/test_vs.glsl
@@ -1,9 +1,7 @@
 #version 410 core
 layout (location = 0) in vec3 vertPos;
-layout (location = 1) in vec3 vertColor;
-layout (location = 2) in vec2 vertTexCoord;
+layout (location = 1) in vec2 vertTexCoord;
 
-out vec3 color;
 out vec2 texCoord;
 
 uniform mat4 model;
