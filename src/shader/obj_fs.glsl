b3673fed7c82a811eba705170c8803a6d6e8b123
Ernest MARIN <emarin@student.42.fr>
2019-08-21 18:55:20 +0200

    customizable material & light properties

--- src/shader/obj_fs.glsl
+++ src/shader/obj_fs.glsl
@@ -1,29 +1,43 @@
 #version 410 core
+struct Material {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shininess;
+};
+
+struct Light {
+	vec3 position;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
 in vec3 normal;
 in vec3 fragPos;
 
 out vec4 fragColor;
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+uniform vec3        viewPos;
+uniform Material    material;
+uniform Light       light;
 
 void main() {
-    float   ambient_strgh = 0.1;
-    vec3    ambient = ambient_strgh * lightColor;
+	// ambient
+	vec3	ambient  = light.ambient * material.ambient;
 
-    vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(lightPos - fragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+	// diffuse
+	vec3	norm = normalize(normal);
+	vec3	lightDir = normalize(light.position - fragPos);
+	float	diff = max(dot(norm, lightDir), 0.0);
+	vec3	diffuse = light.diffuse * (diff * material.diffuse);
 
-    float specular_strgh = 0.5;
-    vec3 viewDir = normalize(viewPos - fragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specular_strgh * spec * lightColor;
+	// specular
+	vec3	viewDir = normalize(viewPos - fragPos);
+	vec3	reflectDir = reflect(-lightDir, norm);
+	float	spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3	specular = light.specular * (spec * material.specular);
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    fragColor = vec4(result, 1.0);
+	vec3	result = ambient + diffuse + specular;
+	fragColor = vec4(result, 1.0);
 }
