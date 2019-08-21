b1836093cb220059dd83238c0011c50b86953375
Ernest MARIN <emarin@e1r4p18.42.fr>
2019-08-21 17:49:24 +0200

    specular lighting

--- src/shader/obj_fs.glsl
+++ src/shader/obj_fs.glsl
@@ -7,6 +7,7 @@ out vec4 fragColor;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main() {
     float   ambient_strgh = 0.1;
@@ -17,6 +18,12 @@ void main() {
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
-    vec3 result = (ambient + diffuse) * objectColor;
+    float specular_strgh = 0.5;
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specular_strgh * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
     fragColor = vec4(result, 1.0);
 }
