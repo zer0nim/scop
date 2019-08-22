ff98e01a8dcdd68117c2cbf8b3bdc4d94509188b
Ernest MARIN <emarin@e1r7p14.42.fr>
2019-08-22 16:36:55 +0200

    directional light

--- src/shader/obj_fs.glsl
+++ src/shader/obj_fs.glsl
@@ -9,7 +9,7 @@ struct Material {
 };
 
 struct Light {
-	vec3			position;
+	vec3			direction;
 	vec3			ambient;
 	vec3			diffuse;
 	vec3			specular;
@@ -29,7 +29,7 @@ void main() {
 
 	// diffuse
 	vec3	norm = normalize(normal);
-	vec3	lightDir = normalize(light.position - fragPos);
+	vec3	lightDir = normalize(-light.direction);
 	float	diff = max(dot(norm, lightDir), 0.0);
 	vec3	diffuse = light.diffuse * diff * texture(material.diffuse, texCoords).rgb;
 
