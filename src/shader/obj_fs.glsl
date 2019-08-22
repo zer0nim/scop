ff84066036a33232722e99bccb6857268f690e75
Ernest MARIN <emarin@e1r7p14.42.fr>
2019-08-22 16:49:54 +0200

    point light

--- src/shader/obj_fs.glsl
+++ src/shader/obj_fs.glsl
@@ -9,10 +9,15 @@ struct Material {
 };
 
 struct Light {
-	vec3			direction;
+	vec3			position;
+
 	vec3			ambient;
 	vec3			diffuse;
 	vec3			specular;
+
+	float			constant;
+	float			linear;
+	float			quadratic;
 };
 
 in vec3				fragPos;
@@ -29,7 +34,7 @@ void main() {
 
 	// diffuse
 	vec3	norm = normalize(normal);
-	vec3	lightDir = normalize(-light.direction);
+	vec3    lightDir = normalize(light.position - fragPos);
 	float	diff = max(dot(norm, lightDir), 0.0);
 	vec3	diffuse = light.diffuse * diff * texture(material.diffuse, texCoords).rgb;
 
@@ -41,6 +46,14 @@ void main() {
 
 	// emission
 	vec3	emission = texture(material.emission, texCoords).rgb;
+
+	float	distance = length(light.position - fragPos);
+	float	attenuation = 1.0 / (light.constant + light.linear * distance + \
+	light.quadratic * (distance * distance));
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
 
 	vec3	result = ambient + diffuse + specular + emission;
 	fragColor = vec4(result, 1.0);
