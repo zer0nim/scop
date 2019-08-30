aa4c26d1526a18d866f153543cf2e11b3a47afbe
Ernest MARIN <emarin@e1r2p18.42.fr>
2019-08-30 12:33:10 +0200

    check grammar instruction

--- src/parser/parse_func_1.c
+++ src/parser/parse_func_1.c
@@ -6,7 +6,7 @@
 /*   By: emarin <emarin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 16:12:37 by emarin            #+#    #+#             */
-/*   Updated: 2019/08/29 19:26:35 by emarin           ###   ########.fr       */
+/*   Updated: 2019/08/30 12:31:54 by emarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,37 @@
 
 int8_t	check_grammar(t_token_l *lst)
 {
-	(void)lst;
+	t_token_func	f_infos;
+	t_token_l		*crnt;
+	int				count;
+
+	f_infos = g_token_func[lst->type];
+	count = 0;
+	crnt = lst->next;
+	while (crnt && crnt->type == f_infos.arg_type && ++count)
+		crnt = crnt->next;
+	if (crnt && crnt->type != f_infos.arg_type && crnt->type != e_comments_t)
+	{
+		fprintf(stderr, "instr %s, bad argument type\n", \
+		g_token_reg[lst->type].name);
+		return (FALSE);
+	}
+	if (count < f_infos.min || (!f_infos.accept_more && count > f_infos.min))
+	{
+		fprintf(stderr, "instr %s need %d%sarguments\n", \
+		g_token_reg[lst->type].name, f_infos.min, \
+		(f_infos.accept_more ? "or more " : ""));
+		return (FALSE);
+	}
 	return (TRUE);
 }
 
 int8_t	parse_vert(t_token_l *lst, t_obj *obj)
 {
-	t_token_l	*crnt;
-
 	printf("vert\n");
 	(void)obj;
-
-	crnt = lst;
 	if (!(check_grammar(lst)))
 		return (FALSE);
-	crnt = lst->next;
-	while (crnt)
-	{
-		printf("type: %s\n", g_token_reg[crnt->type].name);
-		printf("data: %s\n", crnt->data);
-		crnt = crnt->next;
-	}
 	return (TRUE);
 }
 
@@ -46,23 +56,26 @@ int8_t	parse_vert(t_token_l *lst, t_obj *obj)
 int8_t	parse_face(t_token_l *lst, t_obj *obj)
 {
 	printf("face\n");
-	(void)lst;
 	(void)obj;
+	if (!(check_grammar(lst)))
+		return (FALSE);
 	return (TRUE);
 }
 
 int8_t	parse_text_vert(t_token_l *lst, t_obj *obj)
 {
 	printf("text_vert\n");
-	(void)lst;
 	(void)obj;
+	if (!(check_grammar(lst)))
+		return (FALSE);
 	return (TRUE);
 }
 
 int8_t	parse_norm_vert(t_token_l *lst, t_obj *obj)
 {
 	printf("norm_vert\n");
-	(void)lst;
 	(void)obj;
+	if (!(check_grammar(lst)))
+		return (FALSE);
 	return (TRUE);
 }
--- src/parser/parse_func_2.c
+++ src/parser/parse_func_2.c
@@ -6,7 +6,7 @@
 /*   By: emarin <emarin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 16:12:37 by emarin            #+#    #+#             */
-/*   Updated: 2019/08/29 18:30:37 by emarin           ###   ########.fr       */
+/*   Updated: 2019/08/30 12:26:16 by emarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 int8_t	parse_usemtl(t_token_l *lst, t_obj *obj)
 {
 	printf("usemtl\n");
-	(void)lst;
 	(void)obj;
+	if (!(check_grammar(lst)))
+		return (FALSE);
 	return (TRUE);
 }
 
 int8_t	parse_mtllib(t_token_l *lst, t_obj *obj)
 {
 	printf("mtllib\n");
-	(void)lst;
 	(void)obj;
+	if (!(check_grammar(lst)))
+		return (FALSE);
 	return (TRUE);
 }
